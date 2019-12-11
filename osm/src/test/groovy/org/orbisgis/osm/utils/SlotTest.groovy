f927e9d86412930da3d619be14e5521cc19ed985
Sylvain Palominos <sylvain.palominos@univ-ubs.fr>
2019-12-11 08:37:59 +0100

    Fix test on date.

--- osm/src/test/groovy/org/orbisgis/osm/utils/SlotTest.groovy
+++ osm/src/test/groovy/org/orbisgis/osm/utils/SlotTest.groovy
@@ -17,10 +17,12 @@ import static org.junit.jupiter.api.Assertions.*
 class SlotTest {
 
     private static format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private static local = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
 
     @BeforeAll
     static void beforeAll(){
         format.setTimeZone(TimeZone.getTimeZone("Etc/GMT+0"))
+        local.setTimeZone(TimeZone.getTimeZone(ZoneId.systemDefault()))
     }
 
     /**
@@ -44,6 +46,6 @@ class SlotTest {
     void toStringTest(){
         def date = "2019-10-11T13:31:39Z"
         def slot = new Slot("Slot available after: $date, in 3 seconds.")
-        assertEquals "Slot available after: 2019-10-11T15:31:39Z, in 3 seconds.", slot.toString()
+        assertEquals "Slot available after: ${local.format(format.parse(date))}, in 3 seconds.".toString(), slot.toString()
     }
 }
